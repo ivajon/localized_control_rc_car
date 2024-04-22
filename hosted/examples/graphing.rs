899837d503f5ae810f3a29bffda6ea55b4bce45b
ivario123 <ivar.jonsson18@gmail.com>
2024-04-22 13:51:21 +0200

    logging ugly but ye

--- hosted/examples/graphing.rs
+++ hosted/examples/graphing.rs
@@ -465,6 +465,7 @@ impl MockSpi {
                 hold_for_us: 0,
             })
             .collect();
+            println!("Sending {:?}",target);
             spi.spi.write(target.as_slice());
         }
     }
