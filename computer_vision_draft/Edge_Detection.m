ecc04bbdfe89c40e5418b635f2efea99b5c1f62c
Fasken2 <91476287+Fasken2@users.noreply.github.com>
2024-04-22 23:50:27 +0200

    Added comments and cleaned up code

--- computer_vision_draft/Edge_Detection.m
+++ computer_vision_draft/Edge_Detection.m
@@ -1,11 +1,13 @@
-
+% Possible solution for estimating drivable area, safe driving direction
+% and determination of if obstacles/walls are far away or close
 
 imds = imageDatastore("capture1\"); %Get images
-centerPoint = [120,140];
+centerPoint = [120,140]; %Initialization values for centerpoint, arbitrary
 
+%Loop through a directory containing images
 for i = 1:length(imds.Files)
     tic
-    
+    %Read image, correct for rotation of camera and scale down
     image1 = readimage(imds,i);
     image1 = imrotate(image1,5,"crop");
     cropSize = 30;
@@ -13,23 +15,17 @@ for i = 1:length(imds.Files)
     image1 = imresize(image1,1/2);
     image1 = imgaussfilt(image1);
     
-% 
-    
+    %Convert to grayscale and produce binary edge image
     image1 = im2gray(image1);
-
-   edgeY = edge(image1,"prewitt",0.1,"vertical");
    edgeX = edge(image1,"prewitt",0.02,"horizontal");
-   % edgeX = edge(image1,"canny",0.2,0.5);
-   % SE = strel("square",5);
-   % edgeX = imdilate(edgeX,SE);
 
-   % edgeX = imerode(edgeX,SE);
-
+   %Extract lines
    [Hx,T,R] = hough(edgeX,'RhoResolution',1,'Theta',[-89:0.1:-75 75:0.1:89]);
    P  = houghpeaks(Hx,10);
    lines = houghlines(edgeX,T,R,P,'FillGap',10,'MinLength',50);
 
- 
+    %Plot lines across entire image to get binary mask containing stacked
+    %lines
     x = [1 size(image1,2)];
     drivingLimit = zeros(size(image1));
     hold on
@@ -42,6 +38,8 @@ for i = 1:length(imds.Files)
     hold off
     drivingLimit = im2bw(drivingLimit);
     
+    %Step through each column until a line is found, creating a drivability
+    %mask.
     drivableArea = zeros(size(drivingLimit));
     minRow = size(drivingLimit,1);
     for col = 1:size(drivingLimit,2)
@@ -69,57 +67,11 @@ for i = 1:length(imds.Files)
    t  = toc;
    fprintf("%f Hz\n",1/t)
     
-
+      %Overlay drivability mask and display centerpoint as red circle
      figure(1)
      drivabilityImage = labeloverlay(image1,drivableArea);
      drivabilityImage = insertShape(drivabilityImage,"filled-circle",[centerPoint(2),centerPoint(1),5],"Color","red");
      drivabilityImage = insertText(drivabilityImage,[0 0],sprintf("Rows from bottom: %i",size(drivabilityImage,2)-minRow));
      imshow(drivabilityImage,"Border","tight","InitialMagnification",400)
-   
-% 
-%     figure(2)
-%     lX = labeloverlay(image1,edgeX,"Colormap",[1 0 0],"Transparency",0);
-%     imshow(lX)
-% 
-%     figure(3)
-%     imshow(imadjust(rescale(Hx)),'XData',T,'YData',R,...
-%        'InitialMagnification','fit');
-%     title('Limited Theta Range Hough Transform of Gantrycrane Image');
-%     xlabel('\theta')
-%     ylabel('\rho');
-%     axis on, axis normal;
-%     colormap(gca,hot)
-% 
-%     hold on
-%     x = T(P(:,2)); y = R(P(:,1));
-% plot(x,y,'s','color','white');
-% 
-%     hold off
-% 
-
-
-    % figure(4), imshow(image1),
-    % hold on
-    % max_len = 0;
-    % for k = 1:length(lines)
-    %    xy = [lines(k).point1; lines(k).point2];
-    %    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-    % 
-    %    % Plot beginnings and ends of lines
-    %    plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-    %    plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
-    % 
-    %    % Determine the endpoints of the longest line segment
-    %    len = norm(lines(k).point1 - lines(k).point2);
-    %    if ( len > max_len)
-    %       max_len = len;
-    %       xy_long = xy;
-    %    end
-    % end
-    % hold off
-    
-
-   
-
  
 end
