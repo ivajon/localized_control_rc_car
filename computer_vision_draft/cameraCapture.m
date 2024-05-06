87639024ab487de87e4f52b84eb23623de78327c
Viktor Lundström <vikke.lundstrom@gmail.com>
2024-05-06 11:40:29 +0200

    Added .png to gitignore

--- computer_vision_draft/cameraCapture.m
+++ computer_vision_draft/cameraCapture.m
@@ -1,13 +1,26 @@
 cam = webcam(1);
 cam.Resolution = "640x480";
+sampleRate = 10;
+captureTime = 10;
+numImages = sampleRate*captureTime;
+ImageFolder = pwd+"\capture3";
 
-ImageFolder = pwd+"\capture2";
-for k=1:100 % this loop will take 5 pictures and save them in the Matlab folder 
+numberSize = strlength(sprintf("%d",numImages));
+
+for k=1:numImages % this loop will take 5 pictures and save them in the Matlab folder 
     img = snapshot(cam);
-    file_name = sprintf('Image%d.png',k)% name Image with a sequence of number, ex Image1.png , Image2.png....
+    number = sprintf("%d",k);
+    numLen = numberSize-strlength(number);
+    pad = "";
+    for i = 1:numLen
+        pad = append(pad,"0");
+    end
+    file_name = "Image"+pad+number+".png";% name Image with a sequence of number, ex Image1.png , Image2.png....
     %save the image as a Portable Graphics Format file(png)into the MatLab
     imgName = fullfile(ImageFolder,file_name) ;
+    tic
     imwrite(img,imgName);
-    imshow(img) % display the image for every second
-    pause(0.5); % pause for one second
+    toc
+    % imshow(img) % display the image for every second
+    %pause(1/sampleRate); % pause for one second
 end
