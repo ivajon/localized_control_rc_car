96a45152a622f417325996e9d395cf7c0ea03790
ivario123 <ivar.jonsson18@gmail.com>
2024-05-14 22:53:46 +0200

    borked

--- embedded_rtic/src/helpers.rs
+++ embedded_rtic/src/helpers.rs
@@ -27,11 +27,7 @@ pub fn sum(window: &ArrayDeque<f32, SMOOTHING, Wrapping>) -> f32 {
     // let sum = (0..(window.len()))
     // .map(|idx| (idx as f32) / { SMOOTHING as f32 })
     // .sum::<f32>();
-    window
-        .iter()
-        .map(|(_idx, value)| /* (idx as f32) / { SMOOTHING as f32 } */ * value)
-        .sum::<f32>()
-        / window.len() as f32
+    window.iter().sum::<f32>() / window.len() as f32
 }
 
 /// Sends a message on the channel after the given time.
