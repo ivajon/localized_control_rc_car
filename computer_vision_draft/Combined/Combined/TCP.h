4a282218a87fc7535728556ec1832ea2a77a6840
Viktor Lundström <vikke.lundstrom@gmail.com>
2024-05-15 21:54:28 +0200

    1 sock

--- computer_vision_draft/Combined/Combined/TCP.h
+++ computer_vision_draft/Combined/Combined/TCP.h
@@ -3,12 +3,10 @@
 //#include <sys/types.h>
 #pragma once
 //For use on linux?
-#ifdef TCP
 	#include <netinet/in.h>
 	#include <sys/socket.h>
 	#include <unistd.h>
-#endif
 
 
-void TCPclient(char x, char y);
+void TCPclient(char x, char y,int clientSocket);
 
