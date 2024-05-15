ca643ba75cb0586e0b3c08293e46e1aed922e670
Viktor Lundström <vikke.lundstrom@gmail.com>
2024-05-15 10:39:27 +0200

    Work on circle detection

--- computer_vision_draft/Combined/Combined/StartStop.h
+++ computer_vision_draft/Combined/Combined/StartStop.h
@@ -12,11 +12,38 @@ extern Mat hsvImage;
 extern int frameID;
 extern bool stop;
 
-class StartStop {
-public:
-    static int startStop(int LowH, int HighH, int LowS, int HighS, int LowV, int HighV, bool isRed);
-    static int red();
-    static int start();
+/// Struct for storing color limits for cicrle detection in HSV space
+struct colorLimitsHSV {
+    int iLowH;
+    int iHighH;
+
+    int iLowS;
+    int iHighS;
+
+
+    int iLowV;
+    int iHighV;
 };
+
+class StartStop {
+private:
+    colorLimitsHSV redLims;
+    colorLimitsHSV greenLims;
+public:
+    /// Constructor
+    StartStop();
+   
+
+    int startStop(int LowH, int HighH, int LowS, int HighS, int LowV, int HighV, bool isRed);
+    int red();
+    int start();
+
+    /// Start functions in main thread
+    std::thread startThread() {
+        return std::thread([this] { this->start(); });
+    }
+
+};
+
 
 #endif // STARTSTOP_H
